9c8128f241167eeb3b45f336a80697b79c849140
Steve Lemieux <50968226+Soufleuse@users.noreply.github.com>
2025-04-04 10:59:15 -0400

    Update TypePenaliteBd.cs

--- ServiceLigueHockeySqlServer/Data/Models/TypePenaliteBd.cs
+++ ServiceLigueHockeySqlServer/Data/Models/TypePenaliteBd.cs
@@ -9,9 +9,6 @@ namespace ServiceLigueHockeySqlServer.Data.Models
     [Table("TypePenalites")]
     public class TypePenalitesBd
     {
-        // IdPartie sera la primary key utilisée pour lire la feuille de pointage
-        // au lieu d'avoir le triplet id equipe hote - id equipe visiteur - date de
-        // la partie, même si les deux informations sont uniques.
         public short IdTypePenalite { get; set; } = default;
         public int NbreMinutesPenalitesPourCetteInfraction { get; set; } = 2;
     }
