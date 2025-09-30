f9e12608a96dfe75a5cf65ed8b144b84ad786012
Steve Lemieux <Steve.Lemieux@mrnf.gouv.qc.ca>
2025-09-30 13:51:07 -0400

    En ien avec batch 2 de GererEquipeJS

--- ServiceLigueHockeySqlServer/Program.cs
+++ ServiceLigueHockeySqlServer/Program.cs
@@ -34,25 +34,13 @@ namespace ServiceLigueHockerSqlServer
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // D�but d�commenter pour d�ployer sur Ubuntu Server
-            // Laisser comment� pour tester en debug
-            /*builder.WebHost.ConfigureKestrel(serverOption =>
-            {
-                serverOption.Listen(IPAddress.Parse("10.0.0.5"), 5000);
-                serverOption.Listen(IPAddress.Parse("127.0.0.1"), 5000);
-            });*/
-            // Fin d�commenter pour d�ployer sur Ubuntu Server
-
             builder.Services.AddDbContext<ServiceLigueHockeyContext>(options => {
-                //var connectionString =  builder.Configuration.GetConnectionString("mysqlConnection");
                 var connectionString = builder.Configuration.GetConnectionString("sqlServerConnection");
-                //var connectionString = builder.Configuration.GetConnectionString("winServer2022Connection");
                 if (string.IsNullOrEmpty(connectionString))
                 {
                     throw new System.Exception("La chaine de connexion est vide.");
                 }
 
-                //options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 30)));
                 options.UseSqlServer(connectionString);
             });
 
@@ -63,20 +51,10 @@ namespace ServiceLigueHockerSqlServer
                     {
                         Func<string, bool> isMonOrigineAllowed = str => { return true; };
                         builder.AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .WithOrigins("http://localhost:12080", "https://localhost:12080", "http://127.0.0.1:12080", "https://127.0.0.1:12080",
+                               .AllowAnyMethod()
+                               .WithOrigins("http://localhost:12080", "https://localhost:12080", "http://127.0.0.1:12080", "https://127.0.0.1:12080",
                                             "http://localhost:12081", "https://localhost:12081", "http://127.0.0.1:12081", "https://127.0.0.1:12081");
-                        //.SetIsOriginAllowed(isMonOrigineAllowed)
-                        //.AllowCredentials()
-                        //builder.WithOrigins("http://localhost:4900", "https://localhost:4900", "https://localhost:7166", "https://127.0.0.1:4900");
-                        //builder.WithHeaders("Content-Type");
-                        //builder.WithMethods("*");
-                        //builder.WithMethods("POST","GET","PUT","OPTIONS");
                     });
-
-                /*options.AddDefaultPolicy(builder => {
-                    builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
-                });*/
             });
 
             var app = builder.Build();
